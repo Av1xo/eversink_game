b1a9e9abfdfbece8c7c066f42f70df780f2505ee
Av1xo <rostyslav.2004.zhurbenko@gmail.com>
2025-10-05 14:42:37 +0300

    task1

--- main.cpp
+++ main.cpp
@@ -102,15 +102,16 @@ int main() {
 
     float vertices[] = 
     {
-     0.5f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        0.5f,  0.5f, 0.0f,
+        0.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        -1.0f,  0.0f, 0.0f,
     };
 
     unsigned int indices[] = {
-        0, 1, 3,
-        1, 2, 3,
+        2, 1, 0,
+        2, 3, 4,
     };
 
     unsigned int VBO, VAO, EBO;
